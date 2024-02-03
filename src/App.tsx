0b2e868e41ddc4dcab754c9c47bbbd43be46fe7e
Denis Krestov <rupo@list.ru>
2024-02-03 13:09:29 +0300

    Add svg-sprite and content images

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import './App.css';
+import React from 'react';
 import styled from 'styled-components';
+import sprite from "./assets/images/sprite.svg";
 
 
 function App() {
     return (
         <div className="App">
             <Title>Welcome to IT-INCUBATOR</Title>
-            <h2>Add deploy</h2>
+            <h2>Начинаем верстать</h2>
+            <svg>
+                <use xlinkHref={`${sprite}#js`}/>
+            </svg>
+            <svg>
+                <use xlinkHref={`${sprite}#ts`}/>
+            </svg>
+            <svg>
+                <use xlinkHref={sprite + '#react'}/>
+            </svg>
+            <svg>
+                <use xlinkHref={sprite + '#r-native'}/>
+            </svg>
+            <svg>
+                <use xlinkHref={sprite + '#styled'}/>
+            </svg>
         </div>
     );
 }
 
-export default App;
+export default App
 
 const Title = styled.h1`
     font-size: 1.5em;
     text-align: center;
-    color: #e91e63;
+    //color: #e91e63;
+    background: linear-gradient(to right, red, blue);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 `;
