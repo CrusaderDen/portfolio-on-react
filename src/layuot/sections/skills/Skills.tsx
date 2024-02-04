e1ae7ecf18b03ebc3ad9f67a9f2e19166a38d78c
Denis Krestov <rupo@list.ru>
2024-02-04 17:59:32 +0300

    Create structures of main and skills

--- src/layuot/sections/skills/Skills.tsx
+++ src/layuot/sections/skills/Skills.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
-import {SectonTitle} from "../../../components/SectonTitle";
+import {SectionTitle} from "../../../components/SectonTitle";
 import {HardSkill} from "./hardSkill/hardSkill";
+import {SoftSkill} from "./softSkill/SoftSkill";
 
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectonTitle>My Hard Skills...</SectonTitle>
+            <SectionTitle>My Hard Skills...</SectionTitle>
             <FlexWrapper gap={'50px'}>
                 <HardSkill iconId={'js'} title={'javascript'}/>
                 <HardSkill iconId={'ts'} title={'typescript'}/>
@@ -17,9 +18,15 @@ export const Skills = () => {
                 <HardSkill iconId={'nest'} title={'nest js'}/>
                 <HardSkill iconId={'styled'} title={'redux'}/>
                 <HardSkill iconId={'git'} title={'git'}/>
-
             </FlexWrapper>
-            <SectonTitle>... And Soft Skills</SectonTitle>
+
+            <SectionTitle>... And Soft Skills</SectionTitle>
+            <FlexWrapper direction={'column'}>
+                <SoftSkill title={'friendliness'} progress={'80%'}/>
+                <SoftSkill title={'sense of humor'} progress={'70%'}/>
+                <SoftSkill title={'persistence'} progress={'90%'}/>
+                <SoftSkill title={'analytical skills'} progress={'85%'}/>
+            </FlexWrapper>
 
         </StyledSkills>
     );
---
+++ src/layuot/sections/skills/softSkill/SoftSkill.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexWrapper} from "../../../../components/FlexWrapper";
+
+
+type SoftSkillPropsType = {
+    title?: string
+    progress?: string
+}
+
+export const SoftSkill = (props: SoftSkillPropsType) => {
+    return (
+        <FlexWrapper direction={'column'}>
+            <SkillTitle>{props.title}</SkillTitle>
+            <SkillBar>
+                <ProgressBar progress={props.progress}/>
+            </SkillBar>
+        </FlexWrapper>
+    );
+};
+
+const SkillTitle = styled.h3`
+
+`
+
+const SkillBar = styled.div`
+    max-width: 900px;
+    height: 18px;
+    border-radius: 9px;
+    background-color: rgb(22, 41, 80);
+`
+
+const ProgressBar = styled.div<SoftSkillPropsType>`
+    width: ${props => props.progress};
+    height: 18px;
+    border-radius: 9px;
+    background: linear-gradient(270.00deg, rgb(19, 173, 199), rgb(105, 120, 209) 66.667%, rgb(148, 93, 214) 100%);
+`
